cd5d60f3baa63176ea6cb6f5da640fc47cc1a097
Sandy <sing.sandeep89@gmail.com>
2022-05-13 19:31:36 +0530

    Scraped shoe data from Myntra

--- myntra.py
+++ myntra.py
@@ -1,7 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 
 driver = webdriver.Chrome(executable_path= r"C:\Users\Sandy\Downloads\chromedriver_win32\chromedriver.exe")
 
@@ -9,21 +9,44 @@ driver.maximize_window()
 driver.get("https://www.myntra.com/")
 driver.implicitly_wait(20)
 driver.find_element(by=By.XPATH, value ='//div/input[@class="desktop-searchBar"]').send_keys("shoes for men")
-time.sleep(10)
+time.sleep(5)
 driver.find_element (by=By.CSS_SELECTOR, value= 'a.desktop-submit').click()
-time.sleep(10)
+time.sleep(5)
 driver.find_element(by=By.XPATH, value = '//li/label[text()="Casual Shoes"]').click()
 time.sleep(40)
 
-shoename = driver.find_elements(by=By.CLASS_NAME, value= "product-brand")
-shoeprice = driver.find_elements(by=By.XPATH, value = '//div[@class="product-price"]')
-description = driver.find_elements(by=By.XPATH, value= '//div/h4[@class = "product-product"]')
+Brand = driver.find_elements_by_class_name("product-brand")
+Price = driver.find_elements_by_xpath('.//div[@class="product-price"]')
+description = driver.find_elements_by_xpath('.//div/h4[@class = "product-product"]')
 
-for name in shoename:
-    print(name.text)
+scraped_data=[]
 
-for price in shoeprice:
-    print(price.text)
+for i in range(len(Brand)):
+    scrap_info = {
+        'Brand' : Brand [i].text,
+        'Price': Price [i]. text,
+        'Description': description [i].text
+    }
+    scraped_data.append(scrap_info)
 
-for desc in description:
-    print(desc.text)
+df = pd.DataFrame(scraped_data)
+
+df.to_csv('Myntra_Scraped_Data.csv', index=False)
+
+#for i in range(len(shoename)):
+
+
+
+
+#with open ('Myntra_CS_Data.csv', 'a') as file:
+    
+ #       file.write(shoename[i].text + ";" + shoeprice[i].text + ";" + description[i].text + "\n")
+
+#for name in shoename:
+ #   print(name.text)
+
+#for price in shoeprice:
+ #   print(price.text)
+
+#for desc in description:
+ #   print(desc.text)
